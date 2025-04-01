f8d5016bceebe57ae71c6a8fb856bc35767dfdcd
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-04-01 18:45:35 +0200

    created foreign key

--- laravel-product-api/database/migrations/2025_04_01_164502_add_category_foreign_key_to_products.php
+++ laravel-product-api/database/migrations/2025_04_01_164502_add_category_foreign_key_to_products.php
@@ -11,16 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->decimal('price', 8, 2);
-
-
-
-
-            $table->timestamps();
+        Schema::table('products', function (Blueprint $table) {
+            //
         });
     }
 
@@ -29,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
     }
 };
