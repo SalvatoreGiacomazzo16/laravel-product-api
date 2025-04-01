471f9e2c957133d00b14e6c212f919225d20aa1a
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-04-01 18:58:19 +0200

    fixed migration issues

--- laravel-product-api/database/migrations/2025_04_01_164502_add_category_foreign_key_to_products.php
+++ laravel-product-api/database/migrations/2025_04_01_164502_add_category_foreign_key_to_products.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            if (!Schema::hasColumn('products', 'category_id')) {
+                $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            }
+
+
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('set null');
         });
+
     }
 
     /**
@@ -22,7 +31,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropForeign(['category_id']);
         });
     }
 };
